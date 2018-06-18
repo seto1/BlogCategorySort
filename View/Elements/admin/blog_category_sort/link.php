ad33a06d0a7fcbdc1cb6d6ee1c0cbbcf951ca0f2
seto <seto@catchup.co.jp>
2018-06-18 23:23:41 +0900

    4.1.1 対応

--- View/Elements/admin/blog_category_sort/link.php
+++ View/Elements/admin/blog_category_sort/link.php
@@ -1,5 +1,13 @@
+<?php
+if (getVersion() >= 4.1) {
+	$linkText = $this->BcBaser->getImg('admin/btn_sort.png', ['alt' => '並び替え', 'class' => 'btn']) . '並び替え';
+} else {
+	$linkText = $this->BcBaser->getImg('admin/btn_sort.png', ['alt' => '並び替え', 'class' => 'btn']);
+}
+$sortLink = $this->BcBaser->getLink($linkText, ['plugin' => 'blog_category_sort', 'controller' => 'blog_category_sort', 'action' => 'index', $blogContent['BlogContent']['id']]);
+?>
 <script>
 	$(function () {
-		$('#ListTable th > .firstChild').append('<?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_sort.png', ['width' => 65, 'height' => 14, 'alt' => '並び替え', 'class' => 'btn']), ['plugin' => 'blog_category_sort', 'controller' => 'blog_category_sort', 'action' => 'index', $blogContent['BlogContent']['id']]) ?>');
+		$('#ListTable th > .firstChild').append('<?php echo $sortLink ?>');
 	});
 </script>
